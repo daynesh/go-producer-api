dd969517c940d8f088201b18be4d2aee711b5ce1
Daynesh Mangal (dsc) <daynesh_mangal@discovery.com>
2017-07-20 00:24:08 -0400

    Added additional checks to travis (#5)
    Added gofmt, vet and golint to travis checks

---
+++ .travis.golint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+RED='\033[0;1;31m'
+NC='\033[0m' # No Color
+
+go get github.com/golang/lint/golint
+
+if [ -n "$(golint ./src/... 2>&1)" ]; then
+    echo -e "${RED}golint detected problems:"
+    golint ./src/...
+    echo -e "${NC}"
+    exit 1
+fi
