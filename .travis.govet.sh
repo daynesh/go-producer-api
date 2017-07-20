280bd27bea372e22540346cf95605dcf1f635a57
daynesh <dm1017us@yahoo.com>
2017-07-20 00:15:04 -0400

    More updates

--- .travis.govet.sh
+++ .travis.govet.sh
@@ -5,7 +5,7 @@ RED='\033[0;1;31m'
 NC='\033[0m' # No Color
 
 if [ -n "$(go tool vet src 2>&1)" ]; then
-    echo -e "${RED}Vet errors found:"
+    echo -e "${RED}vet errors found:"
     for i in `go tool vet src`; do echo $i; done
     echo -e "${NC}"
     exit 1
