ab2afb6c0507495f96196183f4425c9e21badac9
daynesh <dm1017us@yahoo.com>
2017-07-20 00:12:46 -0400

    More updates

--- .travis.golint.sh
+++ .travis.golint.sh
@@ -6,7 +6,7 @@ NC='\033[0m' # No Color
 
 if [ -n "$(golint ./src/... 2>&1)" ]; then
     echo -e "${RED}golint detected problems:"
-    for i in `golint ./src/...`; go echo $i; done
+    for i in `golint ./src/...`; do echo $i; done
     echo -e "${NC}"
     exit 1
 fi
