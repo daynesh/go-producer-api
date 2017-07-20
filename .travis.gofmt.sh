cfd1aa22090cbdb1ca109165318e546b4106b630
daynesh <dm1017us@yahoo.com>
2017-07-19 22:57:24 -0400

    More updates

--- .travis.gofmt.sh
+++ .travis.gofmt.sh
@@ -5,7 +5,8 @@ RED='\033[0;1;31m'
 NC='\033[0m' # No Color
 
 if [ -n "$(gofmt -l src test)" ]; then
-    echo -e "${RED}Go code is not formatted...failing files:${NC}"
-    for i in `gofmt -l src test`; do echo -e "${RED}$i${NC}"; done
+    echo -e "${RED}Go code is not formatted...failing files:"
+    for i in `gofmt -l src test`; do echo -e $i; done
+    echo -e "${NC}"
     exit 1
 fi
