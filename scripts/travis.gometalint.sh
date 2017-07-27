5120a694d0ac81d2a3ef17337803a5b12c31310d
daynesh <dm1017us@yahoo.com>
2017-07-27 11:54:39 -0400

    Increase deadline

--- scripts/travis.gometalint.sh
+++ scripts/travis.gometalint.sh
@@ -7,9 +7,9 @@ NC='\033[0m' # No Color
 go get gopkg.in/alecthomas/gometalinter.v1
 gometalinter.v1 --install
 
-if [ -n "$(gometalinter.v1 ./src/... -D gotype --deadline=60s 2>&1)" ]; then
+if [ -n "$(gometalinter.v1 ./src/... -D gotype --deadline=180s 2>&1)" ]; then
     echo -e "${RED}gometalinter detected problems:"
-    gometalinter.v1 ./src/... -D gotype --deadline=60s
+    gometalinter.v1 ./src/... -D gotype --deadline=180s
     echo -e "${NC}"
     exit 1
 fi
