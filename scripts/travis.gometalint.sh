41bf00bb8eb86edad6cf98d5a2a3d308da31a3ac
daynesh <dm1017us@yahoo.com>
2017-07-27 13:22:42 -0400

    More updates

--- scripts/travis.gometalint.sh
+++ scripts/travis.gometalint.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-set -e
 
 RED='\033[0;1;31m'
 NC='\033[0m' # No Color
 
+# Install metalinter and any of its referenced linters
 go get gopkg.in/alecthomas/gometalinter.v1
 gometalinter.v1 --install
 
-if [ -n "$(gometalinter.v1 ./src/... -D gotype --deadline=180s 2>&1)" ]; then
+OUTPUT="$(gometalinter.v1 ./src/... -D gotype --deadline=180s 2>&1)"
+if [ -n "${OUTPUT}" ]; then
     echo -e "${RED}gometalinter detected problems:"
-    gometalinter.v1 ./src/... -D gotype --deadline=180s
+    echo $OUTPUT
     echo -e "${NC}"
     exit 1
 fi
